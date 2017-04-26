9d9c99c6c342afe2fafb23538d0c356c767e742b
ShuXiao <d3a2s1l@gmail.com>
2017-04-26 19:27:52 +0800

    add efficiency AN mocro

---
+++ boosted_xAna_BZ.C
@@ -0,0 +1,226 @@
+// example code to run 2015 mono-Higgs boosted selections on signal (EXO-16-012)
+
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+#include <TH1D.h>
+#include <TFile.h>
+#include "untuplizer.h"
+#include <TClonesArray.h>
+#include <TLorentzVector.h>
+#include <fstream>
+#include <TCanvas.h>
+#include "string"
+void efferr(float nsig,float ntotal,float factor=1)
+{
+    float eff = nsig/ntotal;
+    float err = sqrt( (1-eff)*eff/ntotal);
+    cout << "efficiency = " << eff*factor << " +- " << err*factor << endl;
+    ofstream myfile;
+    //myfile.open();
+    //myfile << eff*factor << endl;
+    //myfile << err*factor;
+}
+using namespace std;
+vector<string> scanTri(std::string fileName) {
+    TreeReader data(fileName.data());
+    for(Long64_t jEntry=0; jEntry<data.GetEntriesFast() ;jEntry++){
+
+        if (jEntry % 10000 == 0) fprintf(stderr, "Processing event %lli of %lli\n", jEntry + 1, data.GetEntriesFast());
+
+        data.GetEntry(jEntry);
+        std::string* trigName = data.GetPtrString("hlt_trigName");
+        vector<bool> &trigResult = *((vector<bool>*) data.GetPtr("hlt_trigResult"));
+
+        bool passTrigger=false;
+        vector <string> triList;
+        for(unsigned int it=0; it< trigResult.size(); it++) {
+            if (find(triList.begin(),triList.end(),trigName) == triList.end()) triList.push_back(trigName)
+            //cout << thisTrig << endl;
+        } // end of trigger
+    }
+    for (int i=0;i<triList.size();i++) cout << triList[i];
+    return triList
+}
+void boosted_xAna_BZ(std::string inputFile){
+
+    //get TTree from file ...
+    TreeReader data(inputFile.data());
+
+    TString zpmass;
+    zpmass=gSystem->GetFromPipe(Form("file=%s; test1=${file##*_bb_MZp}; test=${test1%%_gq*}; echo \"${test}\"",inputFile.data()));
+    string outputFile=Form("baryonicZp_MZp%s.txt",zpmass.Data());
+
+    TCanvas* c1 = new TCanvas("c1","",889*1.5,768);
+    TH1F* h_ZpPt = new TH1F("h_ZpPt", "ZpPt", 25,0,2500);
+    TH1F* h_ZpEta = new TH1F("h_ZpEta", "ZpEta", 25,0,2500);
+    TH1F* h_ZpJetM = new TH1F("h_ZpJetM", "ZpJetM", 25,0,2500);
+    TH1F* h_higgsPt = new TH1F("h_higgsPt", "higgs Pt", 20,0,2000);
+    TH1F* h_higgsEta = new TH1F("h_higgsEta", "higgs Eta", 30,-3,3);
+    TH1F* h_higgsJetM = new TH1F("h_higgsJetMass", "higgs jet Mass", 24,50,170);
+    TH1F* h_diJetM = new TH1F("h_diJetMass", "dijet reduced Mass", 24,50,170);
+
+    Int_t nPass[20]={0};
+    int Hindex[2]={-1,-1};
+
+    for(Long64_t jEntry=0; jEntry<data.GetEntriesFast() ;jEntry++){
+
+        if (jEntry % 20000 == 0) fprintf(stderr, "Processing event %lli of %lli\n", jEntry + 1, data.GetEntriesFast());
+
+        data.GetEntry(jEntry);
+        //0. has a good vertex
+        int nVtx = data.GetInt("nVtx");
+        if(nVtx<1)continue;
+        nPass[0]++;
+
+        //1. trigger 
+        std::string* trigName = data.GetPtrString("hlt_trigName");
+        vector<bool> &trigResult = *((vector<bool>*) data.GetPtr("hlt_trigResult"));
+
+        bool passTrigger=false;
+        for(unsigned int it=0; it< trigResult.size(); it++) {
+            std::string thisTrig= trigName[it];
+            bool results = trigResult[it];
+            bool tri[10];
+            tri[0] = thisTrig.find("HLT_PFHT650_WideJetMJJ900DEtaJJ1p5")!=std::string::npos;
+            tri[1] = thisTrig.find("HLT_AK8PFHT650_TrimR0p1PT0p03Mass50")!=std::string::npos;
+            tri[2] = thisTrig.find("HLT_AK8PFHT700_TrimR0p1PT0p03Mass50")!=std::string::npos;
+            tri[3] = thisTrig.find("HLT_PFHT800")!=std::string::npos;
+            tri[4] = thisTrig.find("HLT_PFHT900")!=std::string::npos;
+            tri[5] = thisTrig.find("AK8PFJet360")!=std::string::npos;
+            tri[6] = thisTrig.find("HLT_AK8DiPFJet280_200_TrimMass30_BTagCSV_p20")!=std::string::npos;
+            tri[7] = thisTrig.find("AK8DIPFJET280")!=std::string::npos;
+            if ( tri[5] && results==1 )    
+                {
+                    //cout << thisTrig << endl;
+                    passTrigger=true;
+                    break;
+                }
+        } // end of trigger
+
+        if(!passTrigger) continue;
+        nPass[1]++;
+
+        //2. tight PF jet selection
+
+        Float_t* neuHadFrac = data.GetPtrFloat("FATjetNHadEF");
+        Float_t* neuEmFrac = data.GetPtrFloat("FATjetNEmEF");
+        //Float_t* neuCons = data.GetPtrFloat("FATjet_nSV");
+        Float_t* muoFrac = data.GetPtrFloat("FATjetMuoEF");
+        Float_t* chaHadFrac = data.GetPtrFloat("FATjetCHadEF");
+        Int_t* chaMulti = data.GetPtrInt("FATjetCMulti");
+        Float_t* chaEmFrac = data.GetPtrFloat("FATjetCEmEF");
+        
+        if (neuHadFrac[0] > 0.9) continue;
+        if (neuEmFrac[0] > 0.9)  continue;
+        //if (neuCons[0] <= 1) continue;
+        if (muoFrac[0] > 0.8) continue;
+        if (chaHadFrac[0] <= 0) continue;
+        if (chaMulti[0] <= 0) continue;
+        if (chaEmFrac[0] > 0.9) continue;
+        nPass[2]++;
+
+        // select fatjet 
+
+        int nFATJets = data.GetInt("FATnJet");
+        TClonesArray    *fatjetP4 = (TClonesArray*) data.GetPtrTObject("FATjetP4");
+        float   *fatjetPRmassL2L3Corr = data.GetPtrFloat("FATjetPRmassL2L3Corr");
+        float   *FatjetTau1 = data.GetPtrFloat("FATjetPuppiTau1");
+        float   *FatjetTau2 = data.GetPtrFloat("FATjetPuppiTau2");
+        float Tau21 = *FatjetTau2 / *FatjetTau1;
+     
+        vector<float>   *subjetSDPx  =  data.GetPtrVectorFloat("FATsubjetSDPx", nFATJets);
+        vector<float>   *subjetSDPy  =  data.GetPtrVectorFloat("FATsubjetSDPy", nFATJets); // integral number need fix
+        vector<float>   *subjetSDPz  =  data.GetPtrVectorFloat("FATsubjetSDPz", nFATJets);
+        vector<float>   *subjetSDE   =  data.GetPtrVectorFloat("FATsubjetSDE", nFATJets);  // subjet 4 vector
+        vector<bool>    &passFatJetTightID = *((vector<bool>*) data.GetPtr("FATjetPassIDTight"));
+    
+        // take LO and NLO jet
+        int HIndex[2]={-1,-1};
+        for(int ij=0; ij<nFATJets; ij++)
+        {
+            TLorentzVector* thisJet = (TLorentzVector*)fatjetP4->At(ij);
+            if(thisJet->Pt()<300)continue;
+            if(fabs(thisJet->Eta())>2.4)continue;
+            if(!passFatJetTightID[ij])continue;
+            if (HIndex[0]<0) HIndex[0]=ij;
+            else
+            {
+                HIndex[1]=ij;
+                break;
+            }
+        }
+    
+        if(HIndex[1]<0)continue;
+        nPass[3]++;
+        
+        // Jet kinetic selection
+        // select b quarks
+        TLorentzVector* subjetP4[2][2];
+        for(int i=0;i<2;i++){
+            for(int j=0;j<2;j++){
+                subjetP4[i][j]=new TLorentzVector;
+                subjetP4[i][j]->SetPxPyPzE(subjetSDPx[HIndex[i]][j],subjetSDPy[HIndex[i]][j],subjetSDPz[HIndex[i]][j],subjetSDE[HIndex[i]][j]);
+            }
+        }
+        
+        // assume MZp > M_higss
+        TLorentzVector* higgsJet = (TLorentzVector*)fatjetP4->At(HIndex[0]);
+        TLorentzVector* ZpJet = (TLorentzVector*)fatjetP4->At(HIndex[1]);
+        TLorentzVector *diJet = new TLorentzVector();
+        if (higgsJet->M() > ZpJet->M())
+        {
+            higgsJet = (TLorentzVector*)fatjetP4->At(HIndex[1]);
+            ZpJet = (TLorentzVector*)fatjetP4->At(HIndex[0]);
+        }
+        
+        //mass loose cut
+        //if (higgsJet->M() > 150 || higgsJet->M() < 100) continue;
+        *diJet = *higgsJet + *ZpJet;
+       
+        float redMass2Jet = diJet->M() - (higgsJet->M()-125) - (ZpJet->M()-std::stof(zpmass.Data()));
+        if (redMass2Jet < 200) continue;
+        float higgsPt, higgsEta, ZpPt, ZpEta;
+        higgsPt = higgsJet->Pt();
+        higgsEta = higgsJet->Eta();
+        ZpPt = ZpJet->Pt();
+        ZpEta = ZpJet->Eta();
+        if (abs(higgsEta-ZpEta) > 1.3) continue;
+        if (Tau21 > 0.55) continue;
+        nPass[4]++;
+        
+        h_higgsJetM->Fill(higgsJet->M());
+        h_higgsPt->Fill(higgsPt);
+        h_higgsEta->Fill(higgsEta);
+        h_ZpJetM->Fill(ZpJet->M());
+        h_ZpPt->Fill(ZpPt);
+        h_ZpEta->Fill(ZpEta);
+        h_diJetM->Fill(redMass2Jet); 
+    } // end of loop over entries
+
+    float nTotal = data.GetEntriesFast();
+    std::cout << "nTotal    = " << nTotal << std::endl;
+    for(int i=0;i<20;i++) if(nPass[i]>0) std::cout << "nPass[" << i << "]= " << nPass[i] << std::endl;
+    efferr(nPass[4],nTotal);
+    h_higgsPt->Draw("hist");
+    c1->SaveAs("higgsPt.png");
+    h_higgsEta->Draw("hist");
+    c1->SaveAs("higgsEta.png");
+    h_higgsJetM->Draw("hist");
+    c1->SaveAs("higgsJetM.png");
+    h_ZpPt->Draw("hist");
+    c1->SaveAs("ZpPt.png");
+    h_ZpEta->Draw("hist");
+    c1->SaveAs("ZpEta.png");
+    h_ZpJetM->Draw("hist");
+    c1->SaveAs("ZpJetM.png");
+    h_diJetM->Draw("hist");
+    c1->SaveAs("diJetM.png");
+    /*
+    outputRootFile=Form("%s_boost.root",zpmass.Data());
+    TFile* outFile = new TFile(outputRootFile,"recreate");
+    outFile->Close();
+    */
+}
+
